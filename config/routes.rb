1d3d7c8b8da32648f7e43ed99d425d0d1e7108c1
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-10 17:11:10 -0500

    Add routes, initial view and links to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   devise_for :users
+
+  get 'foods', to: 'foods#index', as: 'foods'
+  get 'recipes', to: 'recipes#index', as: 'recipes'
+  get 'public_recipes', to: 'recipes#show', as: 'public_recipes'
+  get 'general_shoping_list', to: 'recipes#index', as: 'shopping_list'
 end
