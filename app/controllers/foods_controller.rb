23c79b9a58e4ad945d0635625f3210d2628f72ba
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-12 11:32:29 -0500

    Add new food view, create new food method working properly

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,39 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @user = current_user
+    if Food.new(foods_params).invalid?
+      flash[:notice] = 'The Food was not saved for incorrect data'
+      redirect_to "/foods/new/"
+    else
+      @food = Food.create!(foods_params)
+      respond_to do |format|
+        format.html do
+          if @food.save
+            # success message
+            flash[:success] = 'Food saved successfully'
+            # redirect to index
+            redirect_to "/foods/"
+          else
+            # error message
+            flash.now[:error] = 'Error:  Food could not be saved'
+            # render new
+            render :new, locals: { food: @food }
+          end
+        end
+      end
+    end
+  end
+
+  private
+
+  def foods_params
+    params.require(:food).permit(:user_id, :name, :measurement_unit, :price)
+  end
 end
