9c044614226c1ea5a15fae23a2067f227f5fbfe7
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-13 21:08:40 -0500

    Fix linters

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -2,34 +2,32 @@ class RecipeFoodsController < ApplicationController
   def new
     @recipe_food = RecipeFood.new
   end
-  
+
   def create
-    @food_id = Food.find_by(name: params[:recipe_food]["food_id"]).id
-    @recipe_id = params[:recipe_food]["recipe_id"]
-    @quantity = params[:recipe_food]["quantity"]
+    @food_id = Food.find_by(name: params[:recipe_food]['food_id']).id
+    @recipe_id = params[:recipe_food]['recipe_id']
+    @quantity = params[:recipe_food]['quantity']
     @added_recipe_food = RecipeFood.find_by(recipe_id: @recipe_id, food_id: @food_id)
     if @added_recipe_food
       flash[:notice] = 'This Food was already added'
       redirect_to "/recipes/#{params[:recipe_id]}/new"
+    elsif RecipeFood.new(food_id: @food_id, recipe_id: @recipe_id, quantity: @quantity).invalid?
+      flash[:notice] = 'The Recipe was not saved for incorrect data'
+      redirect_to "/recipes/#{params[:recipe_id]}/new"
     else
-      if RecipeFood.new(food_id: @food_id, recipe_id: @recipe_id, quantity: @quantity).invalid?
-        flash[:notice] = 'The Recipe was not saved for incorrect data'
-        redirect_to "/recipes/#{params[:recipe_id]}/new"
-      else
-        @recipe_food = RecipeFood.create!(food_id: @food_id, recipe_id: @recipe_id, quantity: @quantity)
-        respond_to do |format|
-          format.html do
-            if @recipe_food.save
-              # success message
-              flash[:success] = 'Recipe saved successfully'
-              # redirect to index
-              redirect_to "/recipes/#{params[:recipe_id]}/"
-            else
-              # error message
-              flash.now[:error] = 'Error: Recipe could not be saved'
-              # render new
-              render :new, locals: { recipe_food: @recipe_food }
-            end
+      @recipe_food = RecipeFood.create!(food_id: @food_id, recipe_id: @recipe_id, quantity: @quantity)
+      respond_to do |format|
+        format.html do
+          if @recipe_food.save
+            # success message
+            flash[:success] = 'Recipe saved successfully'
+            # redirect to index
+            redirect_to "/recipes/#{params[:recipe_id]}/"
+          else
+            # error message
+            flash.now[:error] = 'Error: Recipe could not be saved'
+            # render new
+            render :new, locals: { recipe_food: @recipe_food }
           end
         end
       end
