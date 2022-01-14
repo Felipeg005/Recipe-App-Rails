cbabe4e81db80533493d8816b9dd4779df331ffc
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-13 20:46:31 -0500

    Add general shopping list and recipe shopping list for each user

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,5 +1,10 @@
-class ShoppingListController < ApplicationController
+class ShoppingListsController < ApplicationController
   def index
     @recipes = Recipe.all
+  end
+
+  def show
+    @recipe_foods = RecipeFood.all
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
